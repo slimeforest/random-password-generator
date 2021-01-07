2630de31f548629e4fa7fa6b94539867576442e3
jack battle <jackonno78@hotmail.com>
2021-01-06 20:26:13 -0600

    updated readme with images

--- Develop/script.js
+++ Develop/script.js
@@ -5,21 +5,19 @@ var copyButton = document.getElementById("copyClipboard");
 const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
 const numChars = "1234567890";
-const speecialChars = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+const specialChars = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
 generateButton.addEventListener("click", generateRandomPassword);
-copyButton.addEventListener("click", copyButton);
 
 function generateRandomPassword() {
 
   let newPassword = "";
   let passwordCharset = "";
 
-  let passwordLengthInput = prompt("How many characters would you like your password to be?");
+  let passwordLengthInput = prompt("How many characters would you like your password to be? Passwords must be between 8 and 128 characters long.");
 
   if (Number(passwordLengthInput < 8) || Number(passwordLengthInput > 128) || isNaN(passwordLengthInput)) {
       alert("Number must be bewteen 8 and 128.");
-      return;
   } 
   let lowercaseCharsInput = confirm("Do you want lowercase letters in your password?")
   if (lowercaseCharsInput){
@@ -35,7 +33,7 @@ function generateRandomPassword() {
   }
   let specialCharsInput = confirm("Do you want special characters to be included in your password?")
   if (specialCharsInput){
-    passwordCharset+=speecialChars
+    passwordCharset+=specialChars
   }
   if (passwordCharset===""){
     alert("You must incluse at least one character type in your password.")
@@ -44,17 +42,13 @@ function generateRandomPassword() {
     for (var i=0; i<passwordLengthInput; i++){
       newPassword += passwordCharset.charAt(Math.floor(Math.random()*passwordCharset.length));
     }
-    localStorage.setItem("password", newPassword);
     document.getElementById("password").value = newPassword
   }
-  console.log(newPassword);
-  
 };
 
 copyButton.addEventListener("click", function(){
   passwordText.select();
   document.execCommand("copy");
-  passwordText.innerHTML = "";
   alert("Copied to clipboard.")
 })
 
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # random-password-generator
-generates random password
+For this homework assignment we were tasked with generating a random password. We had to include some criteria for our password like if you want a certain length, uppercase charcters, lowercase characters, numbers, or special characters to be included in the password.
+
+IMAGES
+![default page, first start up](Assets/screenshots/default.png)
+
+![when you click on "generate password, first prompt](Assets/screenshots/character-length.png)
+
+![asking if you'd like lowercase characters, second prompt](Assets/screenshots/lowercase.png)
+
+![asking if you'd like uppercase characters, third prompt](Assets/screenshots/uppercase.png)
+
+![asking if you'd like numbers in your password, fourth prompt](Assets/screenshots/numbers.png)
+
+![asking if you'd like special characters in your password](Assets/screenshots/special-characters.png)
+
+![the generated password](Assets/screenshots/generated-password.png)
+
+![prompt showing that you copied the new password to your clipboard](Assets/screenshots/copies-to-clipboard.png)
+
+![showing that the copied password does infact copy and posting it into google](Assets/screenshots/copy-paste.png)
